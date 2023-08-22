f503784cbc8289dc5d7fb69c9467e2491b927bc7
kau_newbie <100557836+kau-newbie@users.noreply.github.com>
2023-08-22 00:44:04 -0700

    Doit! JS page.459 added grid & jquery
    maybe a kind of attendance book?

--- req.js
+++ req.js
@@ -5,12 +5,37 @@ xhr.send();
 xhr.onreadystatechange = function(){
     if(xhr.readyState==4 && xhr.status ==200){
         let student = JSON.parse(xhr.responseText);
-
-        const t1= $('<div>').attr('id', 'result');
-        $('body').append(t1);
-        $('#result').html(`
-            <h1>${student.name}</h1>
-            <ul><li>major : ${student.major}</li><li>grade : ${student.grade}</li></ul>
-        `);
+        const row_num = countingArr(student);
+        renderHTML(student);
     }
+}
+
+function renderHTML(contents){
+    
+    let t1= $('<div>').attr('class', 'container');
+    $('body').append(t1);
+
+    for (let content of contents){
+        let output = "";
+        output += `
+            <h2>${content.name}<h2>
+            <ul>
+                <li>major : ${content.major}</li>
+                <li>grade : ${content.grade}</li>
+            </ul>
+            <hr>
+        `;
+        placeItems(output);
+    }
+    
+}
+
+function countingArr(obj){
+    return obj.length;
+}
+
+function placeItems(output){
+    
+    let tmp= $('<div>').attr('class', 'items').html(output);
+    $('.container').append(tmp);
 }
